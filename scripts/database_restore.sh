6daba4bceb8487eaccd3ae55241490b1f2b634c4
ozsari <gokhan.ozsari@chalmers.se>
2024-08-23 08:38:45 +0200

    add files

--- scripts/database_restore.sh
+++ scripts/database_restore.sh
@@ -1,4 +1,4 @@
-# ! /bin/bash
+# !/bin/bash
 # this script will restore a database from a backup file
 
 # Example: ./database_restore.sh 2024-08-01
@@ -9,11 +9,14 @@ if [ ! -f /tmp/.env ]; then
 fi
 source /tmp/.env
 
+BACKUP_FILE=""
 # if the date is not provided as an argument, then find the latest backup file
 if [ -z "$1" ]; then
     BACKUP_FILE=$(ls -t $OUTPUT_DIRECTORY/$DATABASE.*.pg_dump | head -n1)
 else
     BACKUP_FILE=$OUTPUT_DIRECTORY/$DATABASE.$1.pg_dump
 fi
+
+echo "Restoring database from $BACKUP_FILE..."
 
 su $DATABASE_ADMIN -c "pg_restore -Fc -d $DATABASE $BACKUP_FILE" || handle_error
